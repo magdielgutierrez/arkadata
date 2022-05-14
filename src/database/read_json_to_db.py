8204260659779a5d8a62176174af9003ef7a7800
magdielgutierrez.local <mgutierreztwp@hotmail.com>
2022-05-13 22:52:12 -0500

    DataFrame listos , solo de guardar a MySQL Tables

--- src/database/read_json_to_db.py
+++ src/database/read_json_to_db.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd 
 import simplejson as json
 import pgeocode
+import time
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="geoapiExercises")
 data = pgeocode.Nominatim('MX')
@@ -10,27 +11,33 @@ data = pgeocode.Nominatim('MX')
 # Opening JSON file
 file_json = open('./data.json')
 
+# Define  dict_table_records base for table  records in database
+dict_table_records={"id":[],"date_updated":[],"vehicle_id":[],
+                    "vehicle_label":[],"vehicle_status":[],"geographic_point":[],
+                    "position_odometer":[],"position_speed":[]}
+
+# Define  dict_table_ubication base for table  ubication in database
+dict_table_ubication={"vehicle_id":[],"postal_code":[],"highway":[],
+                      "road":[],"neighbourhood":[] }
+
+# List receiving geographic_point values by search ubication
+list_geographic_point=[]
+
+# Inicialize Dataframe , receive postal code values series
+df_list_Value_codezip=pd.DataFrame(dtype = 'object')
+
+
 # returns JSON object as dictionary
 dict_data = json.load(file_json)
 
+# Select key by create new list_result and list_records
 for key, value in dict_data.items():
     list_result = dict_data["result"]
 
 for key, value in list_result.items():
     list_records = list_result["records"]
 
-
-dict_table_records={"id":[],"date_updated":[],"vehicle_id":[],
-                    "vehicle_label":[],"vehicle_status":[],"geographic_point":[],
-                    "position_odometer":[],"position_speed":[]}
-
-dict_table_ubication={"vehicle_id":[],"postal_code":[],"alcaldia_name":[],
-                    "highway":[],"road":[],"neighbourhood":[] }
-
-list_geographic_point=[]
-
-df_list_Value_codezip=pd.DataFrame(dtype = 'object')
-
+# Action by create dict_table_records using dict_data JSON 
 for record in list_records:
     for key,value in record.items():   
         if key == "id":                 
@@ -60,26 +67,32 @@ for record in list_records:
             dict_table_records['position_speed'].append(value)
 
 
-# #pass dict_table_records to df_raw_table_records
-# df_raw_table_records = pd.DataFrame.from_dict(dict_table_records,orient='index')
-# df_raw_table_records = df_raw_table_records.T
-# print(df_raw_table_records)
+# Pass dict_table_records to df_raw_table_records
+df_raw_table_records = pd.DataFrame.from_dict(dict_table_records,orient='index')
+df_raw_table_records = df_raw_table_records.T
+#print(df_raw_table_records.head(5))
 
 count = 0
+
 for point in list_geographic_point:
     
-    if count < 2:
+    if count < 50:
         location = geolocator.reverse(point)
         address = location.raw['address']
         #print("\n",type(address))
+        time.sleep(1)
         for key,value in address.items(): 
             #print(key," -- ", value,"\n")
             if key == "postcode":                 
                 dict_table_ubication['postal_code'].append(value)
+                
                 serie_for_search_code= data.query_postal_code(value)
+                time.sleep(1)
                 df_for_search_code = serie_for_search_code.to_frame()               
                 df_for_search_code=df_for_search_code.T
+              
                 df_for_search_code.drop(columns=['accuracy','latitude','longitude'],inplace=True)
+                
                 
                 #concat df_list_Value_codezip +  df_for_search_code 
                 df_list_Value_codezip = pd.concat([df_list_Value_codezip, df_for_search_code], ignore_index = True, axis = 0)
@@ -92,24 +105,21 @@ for point in list_geographic_point:
 
             if key == "neighbourhood":                 
                 dict_table_ubication['neighbourhood'].append(value)
-
-  
-        
-        
         count+=1
     else:break
 
-#print(df_list_Value_codezip)
 
-#pass dict_table_ubication to df_raw_table_ubication
+
+# Pass dict_table_ubication to df_raw_table_ubication
 df_raw_table_ubication = pd.DataFrame.from_dict(dict_table_ubication,orient='index')
 df_raw_table_ubication=df_raw_table_ubication.T
 #print(df_raw_table_ubication)
 
 df_table_ubication= df_raw_table_ubication.merge(df_list_Value_codezip, on='postal_code')
+df_table_ubication.rename(columns = {'county_name':'alcaldia_name'}, inplace = True)
 print(df_table_ubication)
 
-df_table_ubication.info(verbose=True)
+#df_table_ubication.info(verbose=True)
 
 
 # # Closing file
